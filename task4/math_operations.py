6eaaa0f698ede323945149be92b796b4509a0f56
Natanim Teshome <118844475+NatanimT@users.noreply.github.com>
2023-12-12 18:44:58 +0300

    math_operations.py

--- task4/math_operations.py
+++ task4/math_operations.py
@@ -1,21 +1,27 @@
+def basic_operations(a, b):
+    try:
+        addition = a + b
+        subtraction = a - b
+        multiplication = a * b
+        division = a / b
+        return {
+            'addition': addition,
+            'subtraction': subtraction,
+            'multiplication': multiplication,
+            'division': division
+        }
+    except ZeroDivisionError:
+        return "Error: Division by zero is not allowed."
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-from math_operations import basic_operations, power_operations, apply_operations
-# promots the user to enter the numbers
-a = int(input("Enter the first number:"))
-b = int(input("Enter the second number:"))
-c = int(input("enter the amount of modulo:"))
-result_basic = basic_operations(a, b)
-print("Basic operations result:", result_basic)
 
-result_power = power_operations(a, b)
-print("Power Operation Result:", result_power)
+def power_operation(base, exponent, kwargs):
+    try:
+        result = base  exponent
+        if 'modulo' in kwargs:
+            result %= kwargs['modulo']
+        return result
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-result_power_modulo = power_operations(a, b, modulo=c)
-print("Power Operation Result with modulo:", result_power_modulo)
-
-operations = [
-    (lambda x, y: x + y, (a, b))
-    , (lambda x, y: x * y, (a, b))
-]
-result_apply = apply_operations(operations)
-print("Apply operations result:", result_apply)
